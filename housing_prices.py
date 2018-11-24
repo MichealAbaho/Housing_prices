c55f7aff8b02c07a1601b045d66fc78b63ec3ec0
michealabaho <michealabaho265@gmail.com>
2018-11-24 18:15:03 +0000

    identifying best parameters

--- housing_prices.py
+++ housing_prices.py
@@ -18,177 +18,181 @@ import scipy.stats as st
 import scipy.special as st2
 import matplotlib.pyplot as plt
 import seaborn as sns
+import warnings
 #import xgboost as xgb
 
+#Surpress warnings from python pandas library
+for i in [FutureWarning, DeprecationWarning]:
+    warnings.simplefilter(action='ignore', category=i)
 
 class housing_prices:
     
     def __init__(self, train_dset, test_dset):
         self.__train_dset = pd.read_csv(train_dset)
         self.__test_dset = pd.read_csv(test_dset)
-    
-    def data_prep(self):
-        train = self.__train_dset
-        train.set_index('Id', inplace=True)
-       
-        #missing values
-        miss_per_col = train.isnull().sum().sort_values(ascending=False)
-        miss_per_col[miss_per_col > 0].plot.bar()
-        percent_miss = miss_per_col/train.isnull().count().sort_values(ascending=False)
-        missing = pd.concat([miss_per_col, percent_miss], keys=['Total_missing', 'Percent'], axis=1)
-        
-        #Check the distribution of the SalePrice
-        sns.distplot(train.SalePrice, kde=True)
-        
-        #check the number of purchases recorded per year
-        plt.bar(train.YrSold.value_counts().index, train.YrSold.value_counts(), label='Purchases per year')
-        plt.title('Purchases per year')
-        plt.xlabel('Year Sold')
-        plt.ylabel('# of houses')
-        plt.show()
-        
-        #Check which House prices per year
-        yr_price = train[['YrSold','SalePrice']]
-        yr_price = yr_price.groupby(['YrSold'], as_index=False)['SalePrice'].sum()
-        new_vals = [i for i in range(max(yr_price.YrSold)+1, (max(yr_price.YrSold) + 2))]
-        newf = pd.DataFrame(new_vals, columns=['YrSold'])
-        yr_pri = pd.concat([yr_price, newf])
-        yr_pri.set_index('YrSold', inplace=True)
-        yr_pri.fillna(0, inplace=True)
-        rolling = pd.rolling_mean(yr_pri, window=2)
-        yr_pri.plot()
-        rolling.plot()
-        plt.title('Price per annum')
-        plt.xlabel('Year_sold')
-        plt.ylabel('SalePrice in mlns')
-        plt.show()
-        
-        #check the relationship between LotArea and SalePrice
-        plt.scatter(train.LotArea, train.SalePrice)
-        plt.title('LotArea Vs SalePrice')
-        plt.show()
-        
-        #check the relationship between TotalBsmtSF and SalePrice
-        plt.scatter(train.TotalBsmtSF, train.SalePrice)
-        plt.title('Total Bsmt SF Vs SalePrice')
-        plt.show()
-        
-        #check the relationship between GrLivArea and SalePrice
-        plt.scatter(train.GrLivArea, train.SalePrice)
-        plt.title('GrLivArea Vs SalePrice')
-        plt.show()
-        
-        #check the relationship between YrBuilt(Categroical feature) and SalePrice
-        yr_built = train[['SalePrice', 'YearBuilt']]
-        fig, ax = plt.subplots(figsize=(25,8))
-        fig=sns.boxplot(x='YearBuilt', y='SalePrice', data=yr_built)
-        fig.axis(ymin=0, ymax=800000)
-        plt.xticks(rotation=90)
-        plt.show()
-        
-        #overall qualit
-        ovr_qual = train[['OverallQual','SalePrice']]
-        fig,ax=plt.subplots(figsize=(8,8))
-        sns.boxplot(x='OverallQual', y='SalePrice', data=ovr_qual)
-        plt.show()
-        
-        #checkout the correlation of the data 
-        train_set_corr = train.corr()
-        fig, ax = plt.subplots(figsize=(12,9))
-        sns.heatmap(train_set_corr, linecolor='blue', square=True)
-        plt.show()
-        #most highly correlated variables to SalePrice
-        k = 10
-        cols = train_set_corr.nlargest(k, 'SalePrice')['SalePrice'].index
-        cm = np.corrcoef(train[cols].values.T)
-        sns.heatmap(cm, square=True, cbar=True, annot=True,fmt='.2f', xticklabels=cols.values, yticklabels=cols.values)
-        plt.show()
-        
-        
-        
-        #check normality of dependent variable
-        sns.distplot(train.SalePrice, kde=True, fit=st.norm)
-        fig = plt.figure()
-        fig = st.probplot(train.SalePrice, plot=plt)
-        
-        #check normality of independent variables
-        sns.distplot(train.GrLivArea, kde=True, fit=st.norm)
-        fig = plt.figure()
-        fig = st.probplot(train.GrLivArea, plot=plt)
-        
-        sns.distplot(train.TotalBsmtSF, kde=True, fit=st.norm)
-        fig = plt.figure()
-        fig = st.probplot(train.TotalBsmtSF, plot=plt)
-        
-        
-        return missing
-        
-        
-    def study_data(self):
-        #study your dataset
-        self.__train_dset.describe()
-        #check how many missing values exist per column
-        self.__train_dset.isnull().sum()
-        self.__train_dset[self.__train_dset.isnull().any(axis=1)]
-        #check percentage of missing values
-        percentage_missing_values = self.__train_dset.isnull().sum().sum()/np.product(self.__train_dset.shape)
-        print('%d'%round(percentage_missing_values * 100)+'%')
-        #visualize missing values
-        self.__train_dset.isnull().sum()[self.__train_dset.isnull().sum() > 0].plot.bar()
-        #visulaize the distribution of the data
-        sns.distplot(self.__train_dset['SalePrice'], kde=False, fit=st.norm)
-        #check some insights within the data
-
-        train.YrSold.value_counts().plot.bar()
-        #check which numeric variables have a normal distribution
-        quantitative_vars = [col for col in self.__train_dset.columns if self.__train_dset.dtypes[col] != 'object']
-                
-        normal = pd.DataFrame(self.__train_dset[quantitative_vars])
-        normal.drop(['Id', 'SalePrice'], axis=1, inplace=True) #Your not interested in normality of Id's and the Target or dependent variable
-        test_normality = lambda x: st.shapiro(x.fillna(0))[1] < 0.01
-        normal = normal.apply(test_normality)
-        print(normal)
-        f = pd.melt(self.__train_dset, value_vars=quantitative_vars)
-        g = sns.FacetGrid(f, col="variable",  col_wrap=2, sharex=False, sharey=False)
-        g.map(sns.distplot, 'value')
-        
-        
-        #check which categorical variables are normally distributed using box plots
-        qualitative_vars = [col for col in self.__train_dset.columns if self.__train_dset.dtypes[col] == 'object']
-
-        #read the various groups/vcategories as type category
-        for c in qualitative_vars:
-            self.__train_dset[c] = self.__train_dset[c].astype('category')
-            if self.__train_dset[c].isnull().any():
-                self.__train_dset[c] = self.__train_dset[c].cat.add_categories(['MISSING'])
-                self.__train_dset[c].fillna('MISSING')
-        
-        f = pd.melt(self.__train_dset, id_vars = self.__train_dset['SalePrice'], value_vars=qualitative_vars)
-        g = sns.FacetGrid(f, col="variable",  col_wrap=2, sharex=False, sharey=False, size=5)
-        g.map(sns.boxplot, 'value', 'SalePrice')
-        
-        #update numerical variables
-        num_vars = [var for var in train_set.columns if train_set[var].dtypes != object]
-        normality_dict = {}
-        alpha = 0.01
-        for var in num_vars:
-            if(var != 'Id'):
-                stat, p_value = sp.shapiro(train_set[var])
-                if(p_value < alpha):
-                    normality_dict[var] = True
-                else:
-                    normality_dict[var] = False
-        normality_table = pd.DataFrame([(key, round(value,5)) for key, value in normality_dict.items()], columns=['Factor', 'Normal-Distribution'])
-        normality_table
-        
-        def boxplot(x,y,**kwargs):
-            sns.boxplot(x=x,y=y)
-            x=plt.xticks(rotation=90)
-            
-        return quantitative_vars
-    
-    
-        #quantitative_vars = [var for var in self.__train_dset if self.__train_dset.dtypes[var] != object and var != 'Id']
+    #
+    # def data_prep(self):
+    #     train = self.__train_dset
+    #     train.set_index('Id', inplace=True)
+    #
+    #     #missing values
+    #     miss_per_col = train.isnull().sum().sort_values(ascending=False)
+    #     miss_per_col[miss_per_col > 0].plot.bar()
+    #     percent_miss = miss_per_col/train.isnull().count().sort_values(ascending=False)
+    #     missing = pd.concat([miss_per_col, percent_miss], keys=['Total_missing', 'Percent'], axis=1)
+    #
+    #     #Check the distribution of the SalePrice
+    #     sns.distplot(train.SalePrice, kde=True)
+    #
+    #     #check the number of purchases recorded per year
+    #     plt.bar(train.YrSold.value_counts().index, train.YrSold.value_counts(), label='Purchases per year')
+    #     plt.title('Purchases per year')
+    #     plt.xlabel('Year Sold')
+    #     plt.ylabel('# of houses')
+    #     plt.show()
+    #
+    #     #Check which House prices per year
+    #     yr_price = train[['YrSold','SalePrice']]
+    #     yr_price = yr_price.groupby(['YrSold'], as_index=False)['SalePrice'].sum()
+    #     new_vals = [i for i in range(max(yr_price.YrSold)+1, (max(yr_price.YrSold) + 2))]
+    #     newf = pd.DataFrame(new_vals, columns=['YrSold'])
+    #     yr_pri = pd.concat([yr_price, newf])
+    #     yr_pri.set_index('YrSold', inplace=True)
+    #     yr_pri.fillna(0, inplace=True)
+    #     rolling = pd.rolling_mean(yr_pri, window=2)
+    #     yr_pri.plot()
+    #     rolling.plot()
+    #     plt.title('Price per annum')
+    #     plt.xlabel('Year_sold')
+    #     plt.ylabel('SalePrice in mlns')
+    #     plt.show()
+    #
+    #     #check the relationship between LotArea and SalePrice
+    #     plt.scatter(train.LotArea, train.SalePrice)
+    #     plt.title('LotArea Vs SalePrice')
+    #     plt.show()
+    #
+    #     #check the relationship between TotalBsmtSF and SalePrice
+    #     plt.scatter(train.TotalBsmtSF, train.SalePrice)
+    #     plt.title('Total Bsmt SF Vs SalePrice')
+    #     plt.show()
+    #
+    #     #check the relationship between GrLivArea and SalePrice
+    #     plt.scatter(train.GrLivArea, train.SalePrice)
+    #     plt.title('GrLivArea Vs SalePrice')
+    #     plt.show()
+    #
+    #     #check the relationship between YrBuilt(Categroical feature) and SalePrice
+    #     yr_built = train[['SalePrice', 'YearBuilt']]
+    #     fig, ax = plt.subplots(figsize=(25,8))
+    #     fig=sns.boxplot(x='YearBuilt', y='SalePrice', data=yr_built)
+    #     fig.axis(ymin=0, ymax=800000)
+    #     plt.xticks(rotation=90)
+    #     plt.show()
+    #
+    #     #overall qualit
+    #     ovr_qual = train[['OverallQual','SalePrice']]
+    #     fig,ax=plt.subplots(figsize=(8,8))
+    #     sns.boxplot(x='OverallQual', y='SalePrice', data=ovr_qual)
+    #     plt.show()
+    #
+    #     #checkout the correlation of the data
+    #     train_set_corr = train.corr()
+    #     fig, ax = plt.subplots(figsize=(12,9))
+    #     sns.heatmap(train_set_corr, linecolor='blue', square=True)
+    #     plt.show()
+    #     #most highly correlated variables to SalePrice
+    #     k = 10
+    #     cols = train_set_corr.nlargest(k, 'SalePrice')['SalePrice'].index
+    #     cm = np.corrcoef(train[cols].values.T)
+    #     sns.heatmap(cm, square=True, cbar=True, annot=True,fmt='.2f', xticklabels=cols.values, yticklabels=cols.values)
+    #     plt.show()
+    #
+    #
+    #
+    #     #check normality of dependent variable
+    #     sns.distplot(train.SalePrice, kde=True, fit=st.norm)
+    #     fig = plt.figure()
+    #     fig = st.probplot(train.SalePrice, plot=plt)
+    #
+    #     #check normality of independent variables
+    #     sns.distplot(train.GrLivArea, kde=True, fit=st.norm)
+    #     fig = plt.figure()
+    #     fig = st.probplot(train.GrLivArea, plot=plt)
+    #
+    #     sns.distplot(train.TotalBsmtSF, kde=True, fit=st.norm)
+    #     fig = plt.figure()
+    #     fig = st.probplot(train.TotalBsmtSF, plot=plt)
+    #
+    #
+    #     return missing
+    #
+    #
+    # def study_data(self):
+    #     #study your dataset
+    #     self.__train_dset.describe()
+    #     #check how many missing values exist per column
+    #     self.__train_dset.isnull().sum()
+    #     self.__train_dset[self.__train_dset.isnull().any(axis=1)]
+    #     #check percentage of missing values
+    #     percentage_missing_values = self.__train_dset.isnull().sum().sum()/np.product(self.__train_dset.shape)
+    #     print('%d'%round(percentage_missing_values * 100)+'%')
+    #     #visualize missing values
+    #     self.__train_dset.isnull().sum()[self.__train_dset.isnull().sum() > 0].plot.bar()
+    #     #visulaize the distribution of the data
+    #     sns.distplot(self.__train_dset['SalePrice'], kde=False, fit=st.norm)
+    #     #check some insights within the data
+    #
+    #     train.YrSold.value_counts().plot.bar()
+    #     #check which numeric variables have a normal distribution
+    #     quantitative_vars = [col for col in self.__train_dset.columns if self.__train_dset.dtypes[col] != 'object']
+    #
+    #     normal = pd.DataFrame(self.__train_dset[quantitative_vars])
+    #     normal.drop(['Id', 'SalePrice'], axis=1, inplace=True) #Your not interested in normality of Id's and the Target or dependent variable
+    #     test_normality = lambda x: st.shapiro(x.fillna(0))[1] < 0.01
+    #     normal = normal.apply(test_normality)
+    #     print(normal)
+    #     f = pd.melt(self.__train_dset, value_vars=quantitative_vars)
+    #     g = sns.FacetGrid(f, col="variable",  col_wrap=2, sharex=False, sharey=False)
+    #     g.map(sns.distplot, 'value')
+    #
+    #
+    #     #check which categorical variables are normally distributed using box plots
+    #     qualitative_vars = [col for col in self.__train_dset.columns if self.__train_dset.dtypes[col] == 'object']
+    #
+    #     #read the various groups/vcategories as type category
+    #     for c in qualitative_vars:
+    #         self.__train_dset[c] = self.__train_dset[c].astype('category')
+    #         if self.__train_dset[c].isnull().any():
+    #             self.__train_dset[c] = self.__train_dset[c].cat.add_categories(['MISSING'])
+    #             self.__train_dset[c].fillna('MISSING')
+    #
+    #     f = pd.melt(self.__train_dset, id_vars = self.__train_dset['SalePrice'], value_vars=qualitative_vars)
+    #     g = sns.FacetGrid(f, col="variable",  col_wrap=2, sharex=False, sharey=False, size=5)
+    #     g.map(sns.boxplot, 'value', 'SalePrice')
+    #
+    #     #update numerical variables
+    #     num_vars = [var for var in train_set.columns if train_set[var].dtypes != object]
+    #     normality_dict = {}
+    #     alpha = 0.01
+    #     for var in num_vars:
+    #         if(var != 'Id'):
+    #             stat, p_value = sp.shapiro(train_set[var])
+    #             if(p_value < alpha):
+    #                 normality_dict[var] = True
+    #             else:
+    #                 normality_dict[var] = False
+    #     normality_table = pd.DataFrame([(key, round(value,5)) for key, value in normality_dict.items()], columns=['Factor', 'Normal-Distribution'])
+    #     normality_table
+    #
+    #     def boxplot(x,y,**kwargs):
+    #         sns.boxplot(x=x,y=y)
+    #         x=plt.xticks(rotation=90)
+    #
+    #     return quantitative_vars
+    #
+    #
+    #     #quantitative_vars = [var for var in self.__train_dset if self.__train_dset.dtypes[var] != object and var != 'Id']
     
     def clean_data(self):
         train_set, test_set = self.__train_dset, self.__test_dset
@@ -239,44 +243,43 @@ class housing_prices:
                                                             y_values,
                                                             test_size=0.2)
         mods = {'rf':RandomForestRegressor(),
-                'gb':GradientBoostingRegressor(learning_rate=[0.1])}
+                'gb':GradientBoostingRegressor(learning_rate=0.1)}
         
         final_models = []
+
         for model_label, model in mods.items():
-            un_tuned_batch = un_tuned_models(model, X_train.values, Y_train.values, X_test.values)
+            un_tuned_batch = un_tuned_models(model, X_train, Y_train, X_test)
             train_error = un_tuned_batch[1].mean()
-            test_error = np.sqrt(mean_squared_error(Y_test.values, un_tuned_batch[0], multioutput='uniform_average'))
+            test_error = mean_squared_error(Y_test, un_tuned_batch[0], multioutput='uniform_average')
             print(model_label)
-            print('Root mean squared error during training: {}'.format(train_error))
-            print('Root mean squared error during prediction: {}'.format(test_error))
-            
-            tuned_batch = tuned_models(model, X_train.values, Y_train.values, X_test.values)
+            print('mean squared error during training: {}'.format(train_error))
+            print('mean squared error during prediction: {}'.format(test_error))
+
+            tuned_batch = tuned_models(model, X_train, Y_train, X_test)
             train_error = tuned_batch[1].mean()
-            test_error = np.sqrt(mean_squared_error(Y_test, tuned_batch[0], multioutput='uniform_average'))
-            print(model_label)
-            print('Root mean squared error during training: {}'.format(train_error))
-            print('Root mean squared error during prediction: {}'.format(test_error))
-            
+            test_error = mean_squared_error(Y_test, tuned_batch[0], multioutput='uniform_average')
+            print(model_label + 'Tuned-parameters')
+            print('mean squared error during training: {}'.format(train_error))
+            print('mean squared error during prediction: {}'.format(test_error))
+            print('Best parameters: {}'.format(tuned_batch()[2]))
+
         
         
     
 def un_tuned_models(model, x_train, y_train, xtest):
     train_model = model.fit(x_train, y_train)
     y_pred = train_model.predict(xtest)
-    rmse = np.sqrt(-cross_val_score(model, x_train, y_train, scoring='neg_mean_squared_error', cv=5))
-    return y_pred, rmse
+    rmse = (-cross_val_score(model, x_train, y_train, scoring='neg_mean_squared_error', cv=5))
+    return (y_pred, rmse)
         
 
 def tuned_models(model, x_train, y_train, xtest):
-    param={'n_estimators':[100, 500, 1000, 2000], 'max_depth':[10, 15, 20, 25]}
+    param={'n_estimators':[100, 500], 'max_depth':[None, 10, 15]}
     gs = GridSearchCV(model, param, cv=5, n_jobs=-1)
     gs_model = gs.fit(x_train, y_train)
     y_pred = gs_model.predict(xtest)
-    rmse = np.sqrt(-cross_val_score(gs_model, y_train, scoring='neg_mean_squared_error',cv=5))
-    print (pd.DataFrame(gs_model.cv_results_).sort_values('mean_test_score', ascending=False)[0:5])
-    print (gs_model.best_params_)
-    d = gs_model.best_params_
-    return y_pred, rmse
+    rmse = (-cross_val_score(gs_model, x_train, y_train, scoring='neg_mean_squared_error',cv=5))
+    return (y_pred, rmse, gs_model.best_params_)
 
 def encod(elem):
     lb = LabelEncoder()
@@ -289,5 +292,5 @@ def normalizing(frame):
     return normalised_dset
         
 #chicken()           
-x = housing_prices('H:/Mykel/Github/Housing_prices/train.csv', 'H:/Mykel/Github/Housing_prices/test.csv')
+x = housing_prices('train.csv', 'test.csv')
 x.modelling()
